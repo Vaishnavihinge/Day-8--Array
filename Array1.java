5b0ab3eff718aaad4727670d13ab394cd3b283be
Vaishnavihinge <117362673+Vaishnavihinge@users.noreply.github.com>
2026-02-14 18:49:15 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,18 @@
+
+public class Array1 {
+
+    public static void main(String[] args) {
+
+        int arr[] = new int[3]; //Initialization of array
+
+        arr[0] = 10; //Declaration
+        arr[1] = 20;
+        arr[2] = 30;
+
+        System.out.println(arr[0]);  //Accessing elements
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+        System.out.println("Array length : " + arr.length); //Array length
+    }
+}
